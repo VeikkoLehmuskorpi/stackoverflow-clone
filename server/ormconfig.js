ef1afcaa49166ea026bf92a99097540b3580d5e5
Veikko Lehmuskorpi <veikkolehmuskorpi@gmail.com>
2020-09-08 15:04:13 +0300

    Cleanup typeorm config

---
+++ server/ormconfig.js
@@ -0,0 +1,18 @@
+module.exports = {
+  type: process.env.TYPEORM_TYPE,
+  host: process.env.TYPEORM_HOST,
+  port: process.env.TYPEORM_PORT,
+  username: process.env.TYPEORM_USERNAME,
+  password: process.env.TYPEORM_PASSWORD,
+  database: process.env.TYPEORM_DATABASE,
+  synchronize: true,
+  logging: false,
+  entities: ['dist/entity/**/*.js'],
+  migrations: ['dist/migration/**/*.js'],
+  subscribers: ['dist/subscriber/**/*.js'],
+  cli: {
+    entitiesDir: 'src/entity',
+    migrationsDir: 'src/migration',
+    subscribersDir: 'src/subscriber',
+  },
+};
--- server/src/constants.ts
+++ server/src/constants.ts
@@ -1,2 +1,4 @@
+export const __prod__ = process.env.NODE_ENV === 'production';
+
 export const UID_ALPHABET = `ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz`;
 export const UID_LENGTH = 11;
