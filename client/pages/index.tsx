3e6ffd32e4dfd9d163c10e169063d891922a0836
Veikko Lehmuskorpi <veikkolehmuskorpi@gmail.com>
2020-09-26 10:07:57 +0300

    Init frontend project

---
+++ client/pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+const Home = () => {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1>hello world</h1>
+    </div>
+  );
+};
+
+export default Home;
