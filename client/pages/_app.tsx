b7d8e49839fbcf4d5b29c769aec4404e8f00de86
Veikko Lehmuskorpi <veikkolehmuskorpi@gmail.com>
2020-09-26 15:52:01 +0300

    Create urql client

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,7 +1,16 @@
 import 'antd/dist/antd.css';
+import { Provider, createClient } from 'urql';
+
+const client = createClient({
+  url: process.env.NEXT_PUBLIC_GRAPHQL_SERVER_URL,
+});
 
 const MyApp = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <Provider value={client}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 };
 
 export default MyApp;
